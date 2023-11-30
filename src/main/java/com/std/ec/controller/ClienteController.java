9c526a48f8ec2868893bf6c7196ef6450bcefe4a
SantinyOficial <santiunido@gmail.com>
2023-11-30 10:18:26 -0500

    Actualizacion controller

--- src/main/java/com/std/ec/controller/ClienteController.java
+++ src/main/java/com/std/ec/controller/ClienteController.java
@@ -3,8 +3,9 @@ package com.std.ec.controller;
 import com.std.ec.model.entity.Cliente;
 import com.std.ec.service.ICliente;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -14,21 +15,32 @@ public class ClienteController {
     private ICliente clienteService;
 
 
-    public Cliente create(Cliente cliente) {
+    @PostMapping("cliente")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente create(@RequestBody Cliente cliente) {
         return clienteService.save(cliente);
     }
 
+    @PutMapping("cliente")
     public Cliente update(Cliente cliente) {
         return clienteService.save(cliente);
     }
 
-    public void delete(Integer id) {
+    @DeleteMapping("cliente/{id}")
+    public void delete(@PathVariable Integer id) {
         Cliente clienteDelete = clienteService.findById(id);
         clienteService.delete(clienteDelete);
     }
 
-    public Cliente showById(Integer id){
-       return clienteService.findById(id);
+    @GetMapping("cliente/{id}")
+    public ResponseEntity<?> showById(@PathVariable Integer id) {
+        Cliente cliente = clienteService.findById(id);
+        if (cliente != null) {
+            return new ResponseEntity<>(cliente, HttpStatus.OK);
+        } else {
+            String error404 = "Cliente no encontrado";
+            return new ResponseEntity<>(error404,HttpStatus.NOT_FOUND);
+        }
     }
 
 
